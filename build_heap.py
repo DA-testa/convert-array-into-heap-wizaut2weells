711729d00117d4808aea03de432d026870bc17a8
Kevins Gutmanis <kevins.gutmanis@clarity.cx>
2023-03-13 14:03:19 +0200

    Heap1

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,47 @@
 # python3
-
-
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    
+    def heapify(i):
+        nonlocal swaps
+        largest = i
+        left_child = 2*i + 1
+        right_child = 2*i + 2
+        
+        if left_child < n and data[left_child] > data[largest]:
+            largest = left_child
+            
+        if right_child < n and data[right_child] > data[largest]:
+            largest = right_child
+            
+        if largest != i:
+            swaps.append((i, largest))
+            data[i], data[largest] = data[largest], data[i]
+            heapify(largest)
+    
+    # Build heap
+    for i in range(n//2, -1, -1):
+        heapify(i)
+    
     return swaps
 
 
+
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    input_method=input("I or F")
+    if "I" in input_method:
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif "F" in input_method:
+        file_name = input()
+        with open (file_name, 'r'):
+            n = int(file_name.readline())
+            data = list(map(int, file_name.readline().split()))
+    else:
+        print("Wrong input")
+        return
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
