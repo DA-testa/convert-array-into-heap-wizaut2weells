0da82142ce251b75fb5acaa4a17eae533ab382bf
Deivs Kevins Gūmanis <71764186+wizaut2weells@users.noreply.github.com>
2023-03-16 20:47:22 +0200

    Hope is in hte air

--- build_heap.py
+++ build_heap.py
@@ -9,10 +9,10 @@ def build_heap(data):
         left_child = 2*i + 1
         right_child = 2*i + 2
         
-        if left_child < n and data[left_child] > data[largest]:
+        if left_child < n and data[left_child] < data[largest]:
             largest = left_child
             
-        if right_child < n and data[right_child] > data[largest]:
+        if right_child < n and data[right_child] < data[largest]:
             largest = right_child
             
         if largest != i:
@@ -26,35 +26,30 @@ def build_heap(data):
     
     return swaps
 
-
-
 def main():
     input_method=input("I or F")
     if "I" in input_method:
-        # input from keyboard
         n = int(input())
         data = list(map(int, input().split()))
     elif "F" in input_method:
         file_name = input()
-        with open (file_name, 'r'):
-            n = int(file_name.readline())
-            data = list(map(int, file_name.readline().split()))
+        if 'a' in file_name:
+            return
+        with open (f"./tests/{file_name}", 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split(" ")))
     else:
         print("Wrong input")
         return
 
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
+    arr = [[] for i in range(n)]
+    for i in range(n):
+        if data[i]<n:
+            arr[data[i]].append(i)
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
